f1948365ff62f1cfac08cc8e20cfbdf4c7d328f3
Matthew Congrove <mattcongrove@gmail.com>
2025-09-15 13:17:40 -0500

    Prompt tweaks

--- src/agents/example-telegram/index.ts
+++ src/agents/example-telegram/index.ts
@@ -11,11 +11,5 @@ export default async function Agent(
 export const welcome = () => {
   return {
     welcome: `Welcome to the <span style="color: light-dark(#0AA, #0FF);">[NAME]</span> example agent.\n\n### About\n\n[DESCRIPTION]\n\n### Testing\n\n[TEST_INFO]\n\n### Questions?\n\nThe "Help" command is not available for this agent, as it's a platform-specific example.`,
-    prompts: [
-      {
-        data: `Hello, world!`,
-        contentType: 'text/plain',
-      },
-    ],
   };
 };
